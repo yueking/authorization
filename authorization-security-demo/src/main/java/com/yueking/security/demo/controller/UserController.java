c0bce29abf09df4c1851145b9def5a308811fc48
yuewu <yuekinger@gmail.com>
2022-01-03 14:15:05 +0800

    userController

---
+++ authorization-security-demo/src/main/java/com/yueking/security/demo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.yueking.security.demo.controller;
+
+import com.yueking.security.core.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public List<User> query(User query, Pageable pageable) {
+        System.out.println(":"+query);
+        // System.out.println(ReflectionToStringBuilder.toString(query, ToStringStyle.MULTI_LINE_STYLE));
+        // System.out.println(ReflectionToStringBuilder.toString(pageable, ToStringStyle.MULTI_LINE_STYLE));
+        List<User> userList = new ArrayList<>();
+        userList.add(new User());
+        userList.add(new User());
+        userList.add(new User());
+        return userList;
+    }
+}
