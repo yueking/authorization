060c8fe1ee709ee6a970476d622d9342a3786d5a
yuewu <yuekinger@gmail.com>
2022-01-03 16:24:52 +0800

    baseEntity

---
+++ authorization-security-core/src/main/java/com/yueking/security/core/entity/Base.java
@@ -0,0 +1,40 @@
+package com.yueking.security.core.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+public class Base implements Serializable {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private Date createdDate;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false, length = 64)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private Date updatedDate;
+
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 64)
+    private String updatedBy;
+}
