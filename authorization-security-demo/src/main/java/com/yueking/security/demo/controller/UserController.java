fe0ccefbe99847503cb20cbd12a90f1c0f244686
yuewu <yuekinger@gmail.com>
2022-01-04 18:20:07 +0800

    user接口JsonView

--- authorization-security-demo/src/main/java/com/yueking/security/demo/controller/UserController.java
+++ authorization-security-demo/src/main/java/com/yueking/security/demo/controller/UserController.java
@@ -5,9 +5,11 @@ import com.yueking.security.core.entity.Base;
 import com.yueking.security.core.entity.Permission;
 import com.yueking.security.core.entity.User;
 import com.yueking.security.core.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
