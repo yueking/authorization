a5815b2827ec54761e2c68a7e817ee603456ffb3
yuewu <yuekinger@gmail.com>
2022-01-04 16:43:18 +0800

    清理

--- authorization-security-demo/src/main/java/com/yueking/security/DemoApplication.java
+++ authorization-security-demo/src/main/java/com/yueking/security/DemoApplication.java
@@ -2,13 +2,9 @@ package com.yueking.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
-@EntityScan(basePackages = "com.yueking.security.core.entity")
-@EnableJpaRepositories(basePackages = "com.yueking.security.core.repository")
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
