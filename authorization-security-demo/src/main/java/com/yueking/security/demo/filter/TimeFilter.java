025ac2f8e16ee801b27713183f9f4e78fb299557
yuewu <yuekinger@gmail.com>
2022-01-11 11:32:59 +0800

    自定义filter

---
+++ authorization-security-demo/src/main/java/com/yueking/security/demo/filter/TimeFilter.java
@@ -0,0 +1,31 @@
+package com.yueking.security.demo.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+public class TimeFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        System.out.println("time filter is init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("time filter starting...");
+        long start = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest,servletResponse);
+        long time = System.currentTimeMillis() - start;
+        System.out.println("time filter 耗时:"+time);
+        System.out.println("time filter finish");
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+        System.out.println("time filter is destroy");
+    }
+}
