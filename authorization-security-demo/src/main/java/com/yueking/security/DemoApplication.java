8069c1896771a60eb7e0ebb0382351ce718978c3
yuewu <yuekinger@gmail.com>
2022-01-13 16:08:26 +0800

    swagger

--- authorization-security-demo/src/main/java/com/yueking/security/DemoApplication.java
+++ authorization-security-demo/src/main/java/com/yueking/security/DemoApplication.java
@@ -11,6 +11,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 @RestController
 @SpringBootApplication
 // @SpringBootApplication(scanBasePackages="com.yueking.*")
+
 // @EntityScan(basePackages = "com.yueking.security.core.entity")
 // @EnableJpaRepositories(basePackages = "com.yueking.security.core.repository")
 // @ComponentScan(basePackages = {"com.yueking.security.core.service","com.yueking.security.core.base"})
